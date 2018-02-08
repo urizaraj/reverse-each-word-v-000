330171808c66b169a1e1559fdb6777f349d8b150
Alexander Urizar <urizaraj@gmail.com>
2018-02-08 23:24:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(' ')
-  array.collect! do |value|
+  result = array.collect do |value|
     value.reverse
   end
-  array.join(' ')
+  reuslt.join(' ')
 end
